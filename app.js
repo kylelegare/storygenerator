12f0642883e55a70571258c481f6d77433495b3b
Kyle Legare <>
2022-12-11 01:57:39 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const apiKey = "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";
+
+const generateStoryButton = document.getElementById("generate-story-button");
+const generatedStoryDiv = document.getElementById("generated-story");
+
+generateStoryButton.addEventListener("click", async () => {
+  // Retrieve the values entered in the form's text boxes
+  const characterName = document.getElementById("character-name").value;
+  const characterProblem = document.getElementById("character-problem").value;
+  const storyLocation = document.getElementById("story-location").value;
+
+  // Generate the prompt for the story
+  const prompt = `Please write a children's story with the main character named "${characterName}" who has to deal with "${characterProblem}" and the story takes place in "${storyLocation}".`;
+
+  // Use the OpenAI API to generate the story based on the prompt
+  const response = await fetch(
+    `https://api.openai.com/v1/stories/completions?prompt=${prompt}&api_key=${apiKey}`
+  );
+
+  // Check if the response is defined
+  if (response) {
+    // Display the generated story on the web page
+    generatedStoryDiv.innerHTML = response.data.story;
+  }
+});
